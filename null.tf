9d5fa68c678d2ad96cffc181e9ad0728907ca066
hiddenmarten <hiddenmarten@gmail.com>
2025-08-07 21:23:45 +0200

    [oak] use manifests map, fix bug

--- null.tf
+++ null.tf
@@ -1,6 +1,4 @@
 # Tracks Helm template manifests in state to detect changes during terraform plan
 resource "null_resource" "this" {
-  triggers = {
-    template = data.helm_template.this.manifests
-  }
+  triggers = data.helm_template.this.manifests
 }
